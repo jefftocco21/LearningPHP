14910edb5ae6850d19464ce8fba14ec8efde12a4
Jeff Tocco <jeff.tocco@lyonsexpress.com>
2021-03-12 10:52:20 -0500

    Chapter 6 exercises

---
+++ Chapter_6/start.php
@@ -0,0 +1,6 @@
+<?php include 'Templates/header.php'; ?>
+<!--Main Content Start-->
+            <h2>Welcome!</h2>
+            <p>Hello world! This is my PHP website!</p>
+<!--End Main Content-->
+<?php include 'Templates/footer.php'; ?>
