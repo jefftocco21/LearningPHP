731e88adc3a2e580f1e230dbc9c532f04ba6110d
Jeff Tocco <jeff.tocco@lyonsexpress.com>
2021-03-18 09:07:49 -0400

    creating header and footer files in a separate directory

--- globe_bank/public/staff/index.php
+++ globe_bank/public/staff/index.php
@@ -1,29 +1,8 @@
-<!doctype html>
+<?php require_once('../../private/initialize.php'); ?>
+<?php include('../../private/shared/staff_header.php'); ?>
 
-<html lang="en">
-  <head>
-    <title></title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" media="all" href="../stylesheets/staff.css" />
-  </head>
-
-  <body>
-    <header>
-        <h1>GBI Staff Area</h1>
-    </header>
-
-    <navigation>
-        <ul>
-            <li><a href="index.php">Menu</a></li>
-            <li><a href="">List</a></li>
-        </ul> 
-    </navigation>
-    
     <div = id="content">
     </div>
 
-    <footer>
-        &copy; <?php echo date('Y'); ?> Globe Bank
-    </footer>
-  </body>
-</html>
+<?php include('../../private/shared/staff_footer.php'); ?>
+
