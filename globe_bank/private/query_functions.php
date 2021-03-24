0f03af0eb1c8171965cc8f9cb636c8ab0d0cf327
Jeff Tocco <jeff.tocco@lyonsexpress.com>
2021-03-24 14:35:22 -0400

    adding database connection for pages table in pages section

--- globe_bank/private/query_functions.php
+++ globe_bank/private/query_functions.php
@@ -1,11 +1,21 @@
 <?php
     function find_all_subjects(){
         global $db; //should be avoiding globals whenever possible, need to figure out a better way to do this, maybe just leave the query in the page it is needed in.
-
         $sql = "SELECT * FROM subjects ";
         $sql .= "ORDER BY position ASC";
         $result = mysqli_query($db, $sql);
+        //echo $sql; //good way to test sql
+        confirm_result_set($result);
         return $result;
     }
 
+    function find_all_pages(){
+        global $db;
+        $sql = "SELECT * FROM pages ";
+        $sql .= "ORDER BY subject_id ASC, position ASC";
+        $result = mysqli_query($db, $sql);
+        echo $sql; 
+        confirm_result_set($result);
+        return $result; 
+    }
 ?>
