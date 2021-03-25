d9844a9b07891f6e49724fe275bf6d1bcb03e88b
Jeff Tocco <jeff.tocco@lyonsexpress.com>
2021-03-25 12:34:01 -0400

    from options from database data, turning $subjects into assoc array

--- globe_bank/private/query_functions.php
+++ globe_bank/private/query_functions.php
@@ -41,15 +41,15 @@
         }
     }
 
-    function insert_subject($menu_name, $position, $visible) {
+    function insert_subject($subject) {
             global $db;
         
             $sql = "INSERT INTO subjects ";
             $sql .= "(menu_name, position, visible) ";
             $sql .= "VALUES (";
-            $sql .= "'" . $menu_name . "',";
-            $sql .= "'" . $position . "',";
-            $sql .= "'" . $visible . "'";
+            $sql .= "'" . $subject['menu_name'] . "',";
+            $sql .= "'" . $subject['position'] . "',";
+            $sql .= "'" . $visible['visible'] . "'";
             $sql .= ")";
             $result = mysqli_query($db, $sql);
             // For INSERT statements, $result is true/false
