3bc2bce6096517a35e470595d5d017c0a6e730f7
Jeff Tocco <jeff.tocco@lyonsexpress.com>
2021-03-25 09:59:59 -0400

    Creating function to find a single record from the database

--- globe_bank/private/functions.php
+++ globe_bank/private/functions.php
@@ -43,5 +43,18 @@ function is_get_request() {
   return $_SERVER['REQUEST_METHOD'] == 'GET';
 }
 
+function find_subject_by_id($id){
+  global $db;
+
+  $sql = "SELECT * FROM subjects WHERE id ='" . $id . "';";
+  $result = mysqli_query($db, $sql); 
+  confirm_result_set($result);
+
+  $subject = mysqli_fetch_assoc($result);
+  mysqli_free_result($result);
+  return $subject; //returns an assoc array
+
+}
+
 
 ?>
