6a7e99df86cde910b33f9d49555197083e726193
Jeff Tocco <jeff.tocco@lyonsexpress.com>
2021-03-31 12:02:33 -0400

    Collapsing navigation subjects and pages completed

--- globe_bank/private/shared/public_navigation.php
+++ globe_bank/private/shared/public_navigation.php
@@ -1,33 +1,34 @@
 <?php
-  //default values to prevent errors
+  // Default values to prevent errors
   $page_id = $page_id ?? '';
   $subject_id = $subject_id ?? '';
 ?>
+
 <navigation>
-  <?php $nav_subjects = find_all_subjects(); //query function to find all subjects ?>
+  <?php $nav_subjects = find_all_subjects(); //query function to find all subjects?>
   <ul class="subjects">
-    <?php while($nav_subject = mysqli_fetch_assoc($nav_subjects)) { //result set called $nav_subjects specifically for navigation purposes?>
-      <li class="<?php if($nav_subject['id']==$subject_id) {echo "selected";} ?>">
+    <?php while($nav_subject = mysqli_fetch_assoc($nav_subjects)) {  //result set called $nav_subjects specifically for the navigation purposes?>
+      <li class="<?php if($nav_subject['id'] == $subject_id) { echo 'selected'; } ?>">
         <a href="<?php echo url_for('index.php?subject_id=' . h(u($nav_subject['id']))); ?>">
           <?php echo h($nav_subject['menu_name']); ?>
         </a>
 
         <?php if($nav_subject['id'] == $subject_id) { ?>
-        <?php $nav_pages = find_pages_by_subject_id($nav_subject['id']); //query function to find all pages by subject_id ?>
-        <ul class="pages">
-          <?php while($nav_page = mysqli_fetch_assoc($nav_pages)) { //result set called $nav_subjects specifically for navigation purposes?>
-            <li class="<?php if($nav_page['id']==$page_id) {echo "selected";} ?>">
-              <a href="<?php echo url_for('index.php?id=' . h(u($nav_page['id']))); ?>">
-                <?php echo h($nav_page['menu_name']); ?>
-              </a>
-            </li>
-          <?php } // while $nav_pages ?>
-        </ul>
-        <?php mysqli_free_result($nav_pages); ?>
-        <?php } ?>
+          <?php $nav_pages = find_pages_by_subject_id($nav_subject['id']); //query function to find all pages by subject_id?>
+          <ul class="pages">
+            <?php while($nav_page = mysqli_fetch_assoc($nav_pages)) { ?>
+              <li class="<?php if($nav_page['id'] == $page_id) { echo 'selected'; }?>">
+                <a href="<?php echo url_for('index.php?id=' . h(u($nav_page['id']))); ?>">
+                  <?php echo h($nav_page['menu_name']); ?>
+                </a>
+              </li>
+            <?php } // while $nav_pages ?>
+          </ul>
+          <?php mysqli_free_result($nav_pages); ?>
+        <?php } // if($nav_subject['id'] == $subject_id) ?>
+
       </li>
     <?php } // while $nav_subjects ?>
   </ul>
   <?php mysqli_free_result($nav_subjects); ?>
 </navigation>
-
