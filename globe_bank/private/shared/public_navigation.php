8309cebc1d4041275113c2b70f3622d9e7880d86
Jeff Tocco <jeff.tocco@lyonsexpress.com>
2021-03-31 11:58:47 -0400

    Completed collapsing navigation

--- globe_bank/private/shared/public_navigation.php
+++ globe_bank/private/shared/public_navigation.php
@@ -8,10 +8,11 @@
   <ul class="subjects">
     <?php while($nav_subject = mysqli_fetch_assoc($nav_subjects)) { //result set called $nav_subjects specifically for navigation purposes?>
       <li class="<?php if($nav_subject['id']==$subject_id) {echo "selected";} ?>">
-        <a href="<?php echo url_for('index.php'); ?>">
+        <a href="<?php echo url_for('index.php?subject_id=' . h(u($nav_subject['id']))); ?>">
           <?php echo h($nav_subject['menu_name']); ?>
         </a>
 
+        <?php if($nav_subject['id'] == $subject_id) { ?>
         <?php $nav_pages = find_pages_by_subject_id($nav_subject['id']); //query function to find all pages by subject_id ?>
         <ul class="pages">
           <?php while($nav_page = mysqli_fetch_assoc($nav_pages)) { //result set called $nav_subjects specifically for navigation purposes?>
@@ -23,7 +24,7 @@
           <?php } // while $nav_pages ?>
         </ul>
         <?php mysqli_free_result($nav_pages); ?>
-
+        <?php } ?>
       </li>
     <?php } // while $nav_subjects ?>
   </ul>
