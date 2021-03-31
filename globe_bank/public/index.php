ade217a7d536b35291ed269b6cb9cdeb7b61d7d3
Jeff Tocco <jeff.tocco@lyonsexpress.com>
2021-03-31 10:50:10 -0400

    added static homepage to site

--- globe_bank/public/index.php
+++ globe_bank/public/index.php
@@ -8,6 +8,15 @@
 
   <div id="page">
 
+    <?php 
+    //Show the homepage
+    //Homepage is able to:
+    // *be static content (either in this location or through the use of another shared file)
+    // *show the first page from the navigation menu
+    // * be in the database but add code to hide in the nav
+    include(SHARED_PATH . '/static_homepage.php');
+    ?>
+  
   </div>
 
 </div>
