b969b6ed6bb4ca1170e04761b714c15e994c9390
Jeff Tocco <jeff.tocco@lyonsexpress.com>
2021-04-01 15:24:59 -0400

    adding auth_functions.php, and authenticating user access

--- globe_bank/public/staff/login.php
+++ globe_bank/public/staff/login.php
@@ -7,12 +7,41 @@ $password = '';
 
 if(is_post_request()) {
 
-  $username = $_POST['username'] ?? ''; //setting the username for POST global variable
-  $password = $_POST['password'] ?? ''; //setting the password for POST global variable
+  $username = $_POST['username'] ?? '';
+  $password = $_POST['password'] ?? '';
 
-  $_SESSION['username'] = $username;
+  // Validations
+  if(is_blank($username)) {
+    $errors[] = "Username cannot be blank.";
+  }
+  if(is_blank($password)) {
+    $errors[] = "Password cannot be blank.";
+  }
 
-  redirect_to(url_for('/staff/index.php'));
+  // if there were no errors, try to login
+  if(empty($errors)) {
+    // Using one variable ensures that msg is the same
+    $login_failure_msg = "Log in was unsuccessful.";
+
+    $admin = find_admin_by_username($username);
+    if($admin) {
+
+      if(password_verify($password, $admin['hashed_password'])) {
+        // password matches
+        log_in_admin($admin);
+        redirect_to(url_for('/staff/index.php'));
+      } else {
+        // username found, but password does not match
+        $errors[] = $login_failure_msg;
+      }
+
+    } else {
+      // no username found
+      $errors[] = $login_failure_msg;
+    }
+
+  }
+
 }
 
 ?>
