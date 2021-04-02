e5ca2e441f6d26ee02fca2c84611ee740650aa78
Jeff Tocco <jeff.tocco@lyonsexpress.com>
2021-04-02 14:07:42 -0400

    Nested links across show.php, edit.php, delete.php and new.php in subjects

--- globe_bank/public/staff/pages/delete.php
+++ globe_bank/public/staff/pages/delete.php
@@ -26,7 +26,7 @@ if(is_post_request()) {
 
 <div id="content">
 
-  <a class="back-link" href="<?php echo url_for('/staff/pages/index.php'); ?>">&laquo; Back to List</a>
+<a class="back-link" href="<?php echo url_for('/staff/subjects/show.php?=' . (h(u($page['subject_id'])))); ?>">&laquo; Back to Subject Page</a>
 
   <div class="page delete">
     <h1>Delete Page</h1>
